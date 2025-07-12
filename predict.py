ce07508739df96988ee794d57bdd6b913f7e2e1c
Marshall-Ge <1004083966@qq.com>
2025-07-12 15:03:16 +0000

    update

--- predict.py
+++ predict.py
@@ -1,11 +1,15 @@
+import os
+from consts import *
+
+os.environ["HF_ENDPOINT"] = HF_ENDPOINT
 from diffusers import StableDiffusionPipeline
 import torch
 
-model_id = "./sd-naruto-model"
+model_id = "stable-diffusion-v1-5/stable-diffusion-v1-5"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 pipe = pipe.to("cuda")
 
-prompt = "Lebron James with a hat"
-image = pipe(prompt).images[0]  
-    
+prompt = "A toilet with a calm and classical style."
+image = pipe(prompt).images[0]
+
 image.save("result.png")
